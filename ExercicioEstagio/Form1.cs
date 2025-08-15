49db0631b1fe525ed250f26f1f970c8c1f964440
Mathews <baiomathews@gmail.com>
2025-08-15 13:25:28 -0300

    Add project files.

---
+++ ExercicioEstagio/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+
+namespace ExercicioEstagio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            OleDbConnection connection = new OleDbConnection();
+
+            string pathDB = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"auebd.mdb");
+            pathDB = System.IO.Path.GetFullPath(pathDB);
+
+            connection.ConnectionString = $@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={pathDB};";
+
+            try
+            {
+                connection.Open();
+                checkConnection.Text = "Conexão aberta com sucesso!";
+            }
+            catch (Exception ex)
+            {
+                checkConnection.Text = "Erro ao abrir a conexão: " + ex.Message;
+                return;
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
